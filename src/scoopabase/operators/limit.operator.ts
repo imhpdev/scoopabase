a7cc1358cc28448f2d2664cfa13c653c4471469d
Hiren Patel <imhpdev.social@gmail.com>
2021-07-24 10:21:10 -0500

    LINT: fix linting issues

--- src/scoopabase/operators/limit.operator.ts
+++ src/scoopabase/operators/limit.operator.ts
@@ -2,14 +2,14 @@ import { Observable } from 'rxjs';
 
 export const limit = <T>(limit: number) => {
   return (source: Observable<T>) =>
-    new Observable((subscriber) => {
+    new Observable(subscriber => {
       return source.subscribe({
-        next: (v) => {
+        next: v => {
           if (Array.isArray(v)) {
             subscriber.next(v.slice(0, limit));
           }
         },
-        error: (e) => subscriber.error(e),
+        error: e => subscriber.error(e),
         complete: () => subscriber.complete(),
       });
     });
--- src/scoopabase/utils/type-guards.util.ts
+++ src/scoopabase/utils/type-guards.util.ts
@@ -1,4 +1,5 @@
-export const isNotUndefined = <T>(data: T | undefined): data is T => data !== undefined;
+export const isNotUndefined = <T>(data: T | undefined): data is T =>
+  data !== undefined;
 export const isNotNull = <T>(data: T | null): data is T => data !== null;
 export const isTrue = <T>(data: T | false): data is T => data !== false;
 export const isNotEmpty = <T>(data: T[]): data is T[] => data.length > 0;
